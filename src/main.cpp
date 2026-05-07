63fe964ae314d99bc870a8bf542b3fe2b52c25df
Supai Majhi <sameertudu978@gmail.com>
2026-05-07 14:29:19 +0530

    feat: implement type command

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,27 @@
 int main() {
   while(true) {
     std::string command;
+    std::string input;
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
     std::cout << "$ ";
-    std::getline(std::cin, command);
+    std::getline(std::cin, input);
+    command = input.substr(0, 5);
+
     if(command.compare("exit") == 0) {
       break;
-    }else if(command.substr(0, 5) == "echo ") {
-      std::cout << command.substr(5) << "\n";
+    }else if(command.compare("echo ") == 0) {
+      std::cout << input.substr(5) << "\n";
+    }else if(command.compare("type ") == 0) {
+      if(input.substr(5).compare("echo") == 0 || input.substr(5).compare("type") == 0 || input.substr(5).compare("exit") == 0) {
+        std::cout << input.substr(5) << " is a shell builtin\n";
+      }else {
+        std::cout << input.substr(5) << ": not found\n";
+      }
     }else {
-      std::cout << command << ": command not found\n";
+      std::cout << input << ": command not found\n";
     }
   }
 }
