e7a02b4ae6788f840903a52037776acfc530f1ab
Supai Majhi <sameertudu978@gmail.com>
2026-05-07 13:11:39 +0530

    feat: implement command not found message

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 #include <string>
 
 int main() {
+  std::string command;
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
+  std::cin >> command;
+  std::cout << command << ": command not found";
 }
